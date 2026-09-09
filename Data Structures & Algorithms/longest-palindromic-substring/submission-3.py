d32471d291817bd3e5b911d6e6ba8770582fb843
Adrian Demarcy <159478102+dreyze34@users.noreply.github.com>
2026-09-09 20:17:01 +0200

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n == 1:
+            return s
+        indexes = (0, 0)
+        for i in range(n-1):
+            print(i)
+            k = 0
+            while i - k >= 0 and i + k < n and s[i-k] == s[i+k]:
+                k += 1
+            if 2 * (k-1) >= indexes[1] - indexes[0]:
+                indexes = (i-k+1, i+k)
+                print("odd", indexes)
+            k = 0
+            j = i + 1
+            while i - k >= 0 and j + k < n and s[i-k] == s[j+k]:
+                k += 1
+            if 2 * k >= indexes[1] - indexes[0]:
+                indexes = (i-k+1, j+k)
+                print("even", indexes)
+        return s[indexes[0]:indexes[1]]
